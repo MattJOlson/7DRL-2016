2338ddc87335fd06837e898f81236c8aff9cc10e
Jason Estey <esteyj@yahoo.com>
2016-03-05 01:03:14 -0800

    initial stab at component-entity architecture

---
+++ src/sim/Component.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rogue.src.sim
+{
+    class ComponentProperties
+    {
+        public Dictionary<string, object> Values;
+    }
+
+    class Component
+    {
+        protected Entity Entity;
+
+        public bool Enabled;
+
+        ComponentProperties Properties;
+
+        public virtual void Start() {}
+
+        public virtual void Update(float elapsed) {}
+
+        public Component Get<TComponent>()
+            where TComponent : Component
+        {
+            return Entity != null ? Entity.Components.Get<TComponent>() : null;
+        }
+
+        public bool IsSibling(Component other)
+        {
+            return Entity != null && Entity == other.Entity;
+        }
+
+        public virtual void HandleAttachToEntity(Entity entity)
+        {
+            Entity = entity;
+        }
+
+        public virtual void HandleRemoveFromEntity(Entity entity)
+        {
+            Entity = null;
+        }
+    }
+}
---
+++ src/sim/Entity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rogue.src.sim
+{
+    class EntityComponents
+    {
+        private Entity Entity;
+
+        private Dictionary<Type, Component> Components = new Dictionary<Type, Component>();
+
+        public EntityComponents(Entity entity)
+        {
+            Entity = entity;
+        }
+
+        public Component Get<TComponent>()
+            where TComponent : Component
+        {
+            return Components[typeof(TComponent)];
+        }
+
+        public Component Attach<TComponent>()
+            where TComponent : Component, new()
+        {
+            if (Components.ContainsKey(typeof(TComponent)))
+            {
+                return Components[typeof(TComponent)];
+            }
+
+            var newComponent = new TComponent();
+            Components[typeof(TComponent)] = newComponent;
+            newComponent.HandleAttachToEntity(Entity);
+            return newComponent;
+        }
+
+        public Component Attach(Type type)
+        {
+            if (Components.ContainsKey(type))
+            {
+                return Components[type];
+            }
+
+            Component newComponent = Activator.CreateInstance(type) as Component;
+            if (newComponent == null) { return null; }
+            Components[type] = newComponent;
+            newComponent.HandleAttachToEntity(Entity);
+            return newComponent;
+        }
+
+        public bool Remove<TComponent>()
+            where TComponent : Component
+        {
+            Component component = Components[typeof(TComponent)];
+            component.HandleRemoveFromEntity(Entity);
+            return Components.Remove(typeof(TComponent));
+        }
+    }
+
+    class EntityProperties
+    {
+        public Dictionary<Type, ComponentProperties> FromPrefab = new Dictionary<Type, ComponentProperties>();
+
+        public Dictionary<Type, ComponentProperties> FromSpawn = new Dictionary<Type, ComponentProperties>();
+    }
+
+    class Entity
+    {
+        public Entity Parent;
+
+        public EntityComponents Components;
+
+        public EntityProperties Properties;
+
+        public Entity()
+        {
+            Components = new EntityComponents(this);
+            Properties = new EntityProperties();
+        }
+    }
+}
