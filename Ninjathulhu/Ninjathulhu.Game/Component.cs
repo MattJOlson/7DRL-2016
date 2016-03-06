47c8bffb210085af3c07fa74ace083320dce46cd
Jason Estey <esteyj@yahoo.com>
2016-03-05 17:58:20 -0800

    namespace fix

--- Ninjathulhu/Ninjathulhu.Game/Component.cs
+++ Ninjathulhu/Ninjathulhu.Game/Component.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace rogue.src.sim
+namespace Ninjathulhu.Game
 {
     class ComponentProperties
     {
--- Ninjathulhu/Ninjathulhu.Game/Prefab.cs
+++ Ninjathulhu/Ninjathulhu.Game/Prefab.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace rogue.src.sim
+namespace Ninjathulhu.Game
 {
     class Prefab
     {
